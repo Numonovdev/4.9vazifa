876bef23a705e853acfdaea7d18b57c113529881
Numonovdev <ebxuzsher@gmail.com>
2024-06-27 12:00:10 +0300

    fin

---
+++ js/index.js
@@ -0,0 +1,194 @@
+// Masalalar  forEach metodiga oid masalalar
+
+// 1-masala
+// let arr = [1, 2, 3, 4, 15, 87, 92];
+//   arr.forEach(function(value){
+//        let res;
+//       res = value * 2;
+//       console.log(res);
+// })
+
+
+// 2-masala
+
+// let fruits = ['appl', 'banana', 'cherry'];
+
+// fruits.forEach(function(value){
+//      console.log(value.toUpperCase())
+// })
+
+// 3-masala
+
+// let arr = [1, 2, 3, 4, 15, 87, 92];
+// let res = [];
+//   arr.forEach(function(value){
+//       res.push(value += 2);
+//       })
+//      console.log(res);
+
+// 4-masala
+
+
+
+
+
+// Map metodiga oid masalalar
+
+// 1-masala
+
+// let arr = [1, 2, 3, 4];
+
+// let res = arr.map(function(value){
+//      return value * 2;
+//      })
+// console.log(res);
+
+// 2-masala
+
+
+// let fruits = ['appl', 'banana', 'cherry'];
+
+// let res = fruits.map(function(value){
+//      return value.toUpperCase()
+// })
+// console.log(res)
+
+
+// 3-masala
+
+// let arr = [100, 200, 300, 400, 500];
+
+// let res = arr.map(function(value){
+//      return value += 5;
+// })
+// console.log(res)
+
+// 4-masala
+
+// let students = ['ali 85', 'vali 92'];
+
+// let res = students.map(function(value){
+//      return value += 10;
+// })
+// console.log(res)
+
+// filter metodiga oid masalalar
+
+// 1-masala
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8]
+
+// let res = arr.filter(function(value){
+//      return value % 2 == 0;
+// })
+// console.log(res)
+
+// 2-masala
+// let fruits = ['banana', 'apple', 'cherry', 'date']
+
+// let res = fruits.filter(function(value){
+//      if(value.length > 5){
+//           return value
+//      }
+// })
+// console.log(res)
+
+// 3-masala
+
+// let yoshlar = [12, 17, 18, 19, 21]
+
+// let res = yoshlar.filter(function(value){
+//      return value > 18;
+// })
+// console.log(res)
+
+// 4-masala
+
+// find metodiga oid masalalar
+
+// 1-masala
+
+// let arr =  [4, 9, 11, 2, 18]
+
+// let res = arr.find(function(value){
+//      return value > 10;
+// })
+// console.log(res)
+
+// 2-masala
+
+
+
+
+// some/every metodiga oid masalalar
+
+// 1-masala
+
+// let sonlar = [-1, -2, 3, -4];
+
+// let son = sonlar.some(function(value){
+//     return value > 0;
+// })
+// console.log(son)
+
+// 2-masala
+
+// let hayvonlar = ['cat', 'dog', 'elephant', ]
+
+// let res = hayvonlar.some(function(value){
+//      return value.length > 5
+// })
+// console.log(res)
+
+// 3-masala
+
+// let sonlar = [1, 2, 3, 4];
+
+// let son = sonlar.every(function(value){
+//     return value > 0;
+// })
+// console.log(son)
+
+// 4-masala
+
+
+
+// String metodlariga oid masalalar
+
+// 1-masala
+
+
+// let arr = 'hello';
+
+// let son = arr.charAt(3)
+// console.log(son)
+
+// 2-masala
+
+// let arr = 'hello'
+// let son = arr.charCodeAt(1)
+// console.log(son)
+
+// 3-masala
+
+// let a = 'hello'
+// let b = 'world'
+// let res = a.concat(b)
+// console.log(res)
+
+// 4-masala
+// let arr = ['hello, world!']
+
+// console.log( arr.includes('hello, world!'));
+
+// 5-masala
+
+// let arr = ['hello, world!']
+
+// console.log( arr.endsWith(' world!'));
+
+// 6-masala
+function arr(str) {
+        arr.str.indexOf()
+     
+}
